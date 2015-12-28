1a723344aea1053e7aba7502f46a83b59e58d813
Benjin Dubishar <benjin.dubishar@gmail.com>
2015-12-27 19:18:52 -0800

    Initial commit

---
+++ BeerfestMate/BeerFinder/Beer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+
+namespace BeerFinder
+{
+    public class Beer
+    {
+        public double Rating { get; private set; }
+
+        private int ibuValue;
+        public int? IBU
+        {
+            get
+            {
+                return ibuValue == -1 ? (int?)null : ibuValue;
+            }
+        }
+
+        private double abvValue;
+        public double? ABV
+        {
+            get
+            {
+                return abvValue == -1 ? (double?)null : abvValue;
+            }
+        }
+
+        public string Name { get; private set; }
+        public string Brewery { get; private set; }
+        public string Style { get; private set; }
+        public bool InProduction { get; set; }
+
+    public Beer(string name, string brewery, string style, double rating) : this(name, brewery, style, rating, abv: -1, ibu: -1)
+        {
+
+        }
+
+        public Beer(string name, string brewery, string style, double rating, double abv, int ibu)
+        {
+            Name = name;
+            Brewery = brewery;
+            Style = style;
+            Rating = rating;
+            abvValue = abv;
+            ibuValue = ibu;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static Beer CreateFromDom(HtmlNode node)
+        {
+            string name = node.QuerySelector(".name").InnerText.Trim();
+            string brewery = node.QuerySelector(".brewery").InnerText.Trim();
+            string style = node.QuerySelector(".style").InnerText.Trim();
+            double rating = Double.Parse(node.QuerySelector(".num").InnerText.Trim().Trim('(', ')'));
+
+            string abvString = node.QuerySelector(".abv").InnerText.Trim();
+            double abv = abvString.Contains("N/A") ? -1 : Double.Parse(abvString.Remove(abvString.Length - 5, 5));
+
+            string ibuString = node.QuerySelector(".ibu").InnerText.Trim();
+            int ibu = ibuString.Contains("N/A") ? -1 : Int32.Parse(ibuString.Remove(ibuString.Length - 4, 4));
+
+            Beer beer = new Beer(name, brewery, style, rating, abv, ibu);
+            beer.InProduction = node.QuerySelector(".oop") == null;
+
+            return beer;
+        }
+    }
+}
---
+++ BeerfestMate/BeerFinder/MainWindow.xaml.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Fizzler;
+using System.Net;
+using System.IO;
+using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+
+namespace BeerFinder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string queryEndpointUrl = @"https://untappd.com/search?q=";
+        List<Beer> masterBeerList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            masterBeerList = new List<Beer>();
+        }
+
+        private List<string> CleanInput(string fileName)
+        {
+            StreamReader file = new StreamReader(fileName);
+
+            string line;
+
+            List<string> lines = new List<string>();
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Length < 5)
+                    continue;
+
+                if (!line.Contains("\t"))
+                {
+                    line += file.ReadLine();
+                }
+
+                line = line.Trim();
+
+                string[] split = line.Split('\t');
+
+                lines.Add(split[0] + " " + split[1]);
+            }
+
+            file.Close();
+
+            return lines;
+        }
+
+        private async Task<List<Beer>> SearchBeers(List<string> queries)
+        {
+            HttpWebRequest request;
+            HttpWebResponse response;
+
+            List<Beer> allBeers = new List<Beer>();
+
+            for (int i = 0; i < queries.Count; i++)
+            {
+                loadProgress.Value = i * 100 / queries.Count;
+                string query = queries[i];
+                request = (HttpWebRequest)WebRequest.Create(queryEndpointUrl + query.Replace(' ', '+'));
+                response = (HttpWebResponse) await request.GetResponseAsync();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream stream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(stream, response.CharacterSet == null ? null : Encoding.GetEncoding(response.CharacterSet));
+
+                    string data = reader.ReadToEnd();
+
+                    response.Close();
+                    reader.Close();
+
+                    HtmlDocument html = new HtmlDocument();
+                    html.LoadHtml(data);
+                    HtmlNode document = html.DocumentNode;
+                    
+                    IList<Beer> beers = SearchResultsToBeers(document.QuerySelectorAll(".beer-item"));
+
+                    if (beers.Count <= 2)
+                    {
+                        foreach (Beer beer in beers)
+                            allBeers.Add(beer);
+                    }
+                    else // too many search results
+                    {
+                        allBeers.Add(beers[0]);
+                        allBeers.Add(beers[1]);
+                    }
+                }
+            }
+
+            return allBeers;
+        }
+
+        private IList<Beer> SearchResultsToBeers(IEnumerable<HtmlNode> html)
+        {
+            IList<Beer> beers = new List<Beer>();
+
+            foreach (HtmlNode node in html)
+            {
+                Beer beer = Beer.CreateFromDom(node);
+
+                if (beer.InProduction)
+                    beers.Add(beer);
+            }
+
+            return beers;
+        }
+
+        private void apply_Click(object sender, RoutedEventArgs e)
+        {
+            if ((bool)allCheckbox.IsChecked)
+            {
+                beerGrid.ItemsSource = masterBeerList;
+                return;
+            }
+
+            string filterKeyword;
+
+            if ((bool)pilsnerCheckbox.IsChecked)
+                filterKeyword = "pilsner";
+            else if ((bool)wheatCheckbox.IsChecked)
+                filterKeyword = "wheat";
+            else if ((bool)ipaCheckbox.IsChecked)
+                filterKeyword = "ipa";
+            else if ((bool)stoutCheckbox.IsChecked)
+                filterKeyword = "stout";
+            else if ((bool)ciderCheckbox.IsChecked)
+                filterKeyword = "cider";
+            else if ((bool)sourCheckbox.IsChecked)
+                filterKeyword = "sour";
+            else if ((bool)porterCheckbox.IsChecked)
+                filterKeyword = "porter";
+            else
+            {
+                beerGrid.ItemsSource = masterBeerList;
+                return;
+            }
+
+            List<Beer> filteredBeers = new List<Beer>();
+            
+            foreach (Beer beer in masterBeerList)
+                if (beer.Style.ToLower().Contains(filterKeyword))
+                    filteredBeers.Add(beer);
+
+            beerGrid.ItemsSource = filteredBeers;
+        }
+
+        private async void load_Click(object sender, RoutedEventArgs e)
+        {
+            string fileName = @"..\..\BeerList.txt";
+            List<string> lines = CleanInput(fileName);
+            loadProgress.Value = 0;
+            loadProgress.IsEnabled = true;
+            masterBeerList = await SearchBeers(lines);
+            beerGrid.ItemsSource = masterBeerList;
+            loadProgress.Value = 0;
+            loadProgress.IsEnabled = false;
+        }
+
+        private void filter_Click(object sender, RoutedEventArgs e)
+        {
+            apply_Click(sender, e);
+        }
+    }
+}
